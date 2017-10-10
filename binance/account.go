1b2e4c1dc596e8da61d8f2c5f4d2b23887c247c6
Matt Woop <matthew.woop@gmail.com>
2017-10-10 14:59:53 +0000

    adds response.go file w/ response strcuts from account.go

--- binance/account.go
+++ binance/account.go
@@ -7,42 +7,19 @@ package binance
 import (
     "fmt"
     "errors"
-    //"encoding/json"
 )
 
-/*
-var OrderSideEnum = map[string]bool {
-    "BUY":  true,
-    "SELL": true,
-}
 
-var OrderTypeEnum = map[string]bool {
-    "LIMIT":  true,
-    "MARKET": true,
-}
+func (b *Binance) GetAccountInfo() (account Account, err error) {
 
-var OrderTIFEnum = map[string]bool {
-    "GTC": true,
-    "IOC": true,
-}
-*/
+    reqUrl := fmt.Sprintf("v3/account")
 
-// Result from endpoint: GET /api/v3/account
-type Account struct {
-    MakerCommission  int64     `json:"makerCommission"`
-    TakerCommission  int64     `json:"takerCommission"`
-    BuyerCommission  int64     `json:"buyerCommission"`
-    SellerCommission int64     `json:"sellerCommission"`
-    CanTrade         bool      `json:"canTrade"`
-    CanWithdraw      bool      `json:"canWithdraw"`
-    CanDeposit       bool      `json:"canDeposit"`
-    Balances         []Balance `json:"balances"`
-}
+    _, err = b.client.do("GET", reqUrl, "", true, &account)
+    if err != nil {
+        return
+    }
 
-type Balance struct {
-    Asset  string  `json:"asset"`
-    Free   float64 `json:"free,string"`
-    Locked float64 `json:"locked,string"`
+    return
 }
 
 
@@ -69,14 +46,6 @@ func (b *Binance) GetPositions() (positions []Balance, err error) {
     return positions[:i], nil
 }
 
-
-// Result from endpoint: POST /api/v3/order
-type PlacedOrder struct {
-    Symbol        string `json:"symbol"`
-    OrderId       int64  `json:"orderId"`
-    ClientOrderId string `json:"clientOrderId"`
-    TransactTime  int64  `json:"transactTime"`
-}
 
 // Input for endpoint: POST /api/v3/order
 type LimitOrder struct {
@@ -174,14 +143,6 @@ func (b *Binance) PlaceMarketOrder(m MarketOrder) (res PlacedOrder, err error) {
 }
 
 
-// Result from endpoint: DELETE /api/v3/order
-type DeletedOrder struct {
-    Symbol            string `json:"symbol"`
-    OrigClientOrderId string `json:"origClientOrderId"`
-    OrderId           int64  `json:"orderId"`
-    ClientOrderId     string `json:"clientOrderId"`
-}
-
 // Input for endpoint: GET & DELETE /api/v3/order
 type OrderQuery struct {
     Symbol     string
@@ -205,7 +166,7 @@ func (o *OrderQuery) ValidateOrderQuery() error {
 }
 
 
-func (b *Binance) CancelOrder(query OrderQuery) (order DeletedOrder, err error) {
+func (b *Binance) CancelOrder(query OrderQuery) (order CanceledOrder, err error) {
 
     err = query.ValidateOrderQuery()
     if err != nil {
@@ -220,24 +181,6 @@ func (b *Binance) CancelOrder(query OrderQuery) (order DeletedOrder, err error) 
     }
 
     return
-}
-
-
-// Result from endpoint: GET /api/v3/order
-type OrderStatus struct {
-    Symbol        string  `json:"symbol"`
-    OrderId       int64   `json:"orderId"`
-    ClientOrderId string  `json:"clientOrderId"`
-    Price         float64 `json:"price,string"`
-    OrigQty       float64 `json:"origQty,string"`
-    ExecutedQty   float64 `json:"executedQty,string"`
-    Status        string  `json:"status"`
-    TimeInForce   string  `json:"timeInForce"`
-    Type          string  `json:"type"`
-    Side          string  `json:"side"`
-    StopPrice     float64 `json:"stopPrice,string"`
-    IcebergQty    float64 `json:"icebergQty,string"`
-    Time          int64   `json:"time"`
 }
 
 
